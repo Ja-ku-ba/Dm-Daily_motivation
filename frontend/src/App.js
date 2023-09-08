39b0ddc52872c2978fdaffd323ded9a4285b6bad
Kuba kuba <wulfrykpierozek@gmail.com>
2023-09-08 16:04:47 +0200

    chart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./App.scss";
 import React from "react";
 import NavBar from "./components/NavBar";
+import Tracker from "./components/Tracker";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     return (
       <div className="App">
         <NavBar/>
+        <Tracker/>
         <h1 className="test">Ok?</h1>
       </div>
     );
