d9b1c29a3fb2cb35553070aab61ecbf4feb97035
Kuba kuba <wulfrykpierozek@gmail.com>
2023-10-12 10:57:18 +0200

    jwt + login + logout is done

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -9,8 +9,9 @@ export default AuthContext
 
 export const AuthProvider = ({children}) => {
     
-    let [authTokens, setAuthTokens] = useState(localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
-    let [user, setUser] = useState(localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')) : null)
+    let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwtDecode(localStorage.getItem('authTokens')) : null)
+    let [loading, steLoading] = useState(true)
 
     let navigate = useNavigate();
 
@@ -37,12 +38,52 @@ export const AuthProvider = ({children}) => {
         }
     }
 
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        navigate("/login")
+    }
+    
+    let updateToken = async () => {
+        console.log("Weszło")
+        let response = await fetch("/token/refresh/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                'refresh': authTokens.refresh
+            })
+        })
+        let data = await response.json()
+        if (response.status === 200){
+            setAuthTokens(data)
+            setUser(data.access)
+            localStorage.setItem("authTokens", JSON.stringify(data))
+        } else {
+            logoutUser()
+            alert("Coś poszło nie tak")
+        }
+    }
+    
+        useEffect(() => {
+            const timeDelay = 1000 * 60 * 4
+            let interval = setInterval(() => {
+                if(authTokens) {
+                    updateToken()
+                }
+            }, timeDelay)
+            return () => clearInterval(interval)
+        }, [authTokens, loading])
+    
     let contextData = {
         loginUser: loginUser,
+        logoutUser: logoutUser,
         user: user,
     }
-  return(
-    <AuthContext.Provider value={contextData}>
+    return(
+        <AuthContext.Provider value={contextData}>
         {children}
     </AuthContext.Provider>
   )
