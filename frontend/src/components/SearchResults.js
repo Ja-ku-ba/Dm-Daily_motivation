bc8a83eac05bd33e6315057c22b023bc81b1808b
Ubuntu <wulfrykpierozek@gmail.com>
2023-12-07 16:35:04 +0100

    SearchResults component  progres

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import dogo from "../assets/images/dogo.jpg"
 
 const SearchResults = () => {
   return (
-    <div>SearchResults</div>
+    <Link className='SearchResults'>
+        <div className='SearchResults__profile'>
+            <img className='SearchResults__profile__profilePicture' src={dogo} width={"50px"} height={"50px"}></img>
+            <span className='SearchResults__profile__name'>Jhon Bean</span>
+            <span className='SearchResults__profile__tracker'>13 goals in last month</span>
+        </div>
+        <div className='SearchResults__event'>
+            <img className='SearchResults__profile__profilePicture' src={dogo} width={"50px"} height={"50px"}></img>
+            <span className='SearchResults__profile__name'>Jhon Bean</span>
+            <span className='SearchResults__profile__tracker'>13 goals in last month</span>
+        </div>
+        <div className='SearchResults__achivment'>
+            <img className='SearchResults__profile__profilePicture' src={dogo} width={"50px"} height={"50px"}></img>
+            <span className='SearchResults__profile__name'>Jhon Bean</span>
+            <span className='SearchResults__profile__tracker'>13 goals in last month</span>
+        </div>
+    </Link>
   )
 }
 
